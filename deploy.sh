e26325a773d03a538c39adce2575faedec6bad14
Janik Moeller <janik.moeller.1@gmail.com>
2022-06-04 20:04:01 +0200

    new try

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,8 @@
 # Stop script on first error
 set -e
 
-# Decode SSH key
-echo "${SSH_KEY}" | base64 -d > ssh_key
-chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
-
-# Add production server to known hosts
-# echo "${SERVER_PUBLIC_KEY}" | base64 -d >> ~/.ssh/known_hosts
-
 echo "Deploying via remote SSH"
-ssh -v -i ssh_key "yangsudo@144.91.72.53" \
+ssh "yangsudo@144.91.72.53" \
   "docker pull yangmole/portfolio-main:latest \
   && docker stop live-container \
   && docker rm live-container \

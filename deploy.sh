7b86f3203c0c36d2d94291d48cad4757e1fc629d
Janik Moeller <janik.moeller.1@gmail.com>
2022-06-04 20:17:26 +0200

    updated docker commands

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ set -e
 echo "Deploying via remote SSH"
 ssh "yangsudo@144.91.72.53" \
   "docker pull yangmole/portfolio-main:latest \
-  && docker stop live-container \
-  && docker rm live-container \
-  && docker run --init -d --name portfolio-main -e VIRTUAL_HOST=yangmole.com -e LETSENCRYPT_HOST=yangmole.com -e VIRTUAL_PORT=80 --network net yangmole/portfolio-main:latest \
+  && docker stop portfolio \
+  && docker rm portfolio \
+  && docker run --init -d --name portfolio -e VIRTUAL_HOST=yangmole.com -e LETSENCRYPT_HOST=yangmole.com -e VIRTUAL_PORT=80 --network net yangmole/portfolio-main:latest \
   && docker system prune -af" # remove unused images to free up space
 
 echo "Successfully deployed, hooray!"
